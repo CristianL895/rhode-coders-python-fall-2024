025edd15e7434acaac03696c36bc63a976f4a547
CristianL895 <cristianlopez2439@gmail.com>
2024-11-13 17:41:19 -0500

    Updated Grade Calculator.py
    This is an updated version of the Grade Calculator with the extra comments.

--- Grade Calculator.py
+++ Grade Calculator.py
@@ -3,8 +3,10 @@ score1 = int(input("Enter your first grade (0-100): "))
 score2 = int(input("Enter your second grade (0-100): "))
 score3 = int(input("Enter your third grade (0-100): "))
 
+# The "total_score" means that the other 3 scores have been stored in a "list".
 total_score = [score1, score2, score3]
 
+# This "total" will be used to calculate the average of the code.
 total = 0
 
 # You use a "for" loop in order to add up the score the student put in at the top.
@@ -17,7 +19,7 @@ average = total /3
 grade = ()
 
 # This is the calculation where depending on what was your "average", it will give you your grade.
-if 95 <= average == 100:
+if 95 <= average == 100:    # I dicided this grade value because I think the grade should be going down by 5. Each time it does, your score lowers.
         grade = "A+"
 elif 90 <= average < 95:
         grade = "A"
@@ -33,7 +35,7 @@ elif 65 <= average < 70:
         grade = "C"
 elif 60 <= average < 65:
         grade = "D"
-elif average < 60:
+elif average < 60:            # I think 60 and below is an F because, if this was real life, you could have asked for helped if you were strugguling with the material.
         grade = "F"
 
 # These will print out the list of your scores, the "average" AKA your final score, and your final grade
